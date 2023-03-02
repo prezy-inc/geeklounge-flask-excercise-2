663a58335b0da7fd1c71943b87c5308f153eefff
Atsushi <p.taki_atsu_0919@i.softbank.jp>
2023-03-02 15:27:42 +0900

    first commit

---
+++ apps/minimalapp/app.py
@@ -0,0 +1,138 @@
+import logging
+import os
+
+from email_validator import EmailNotValidError, validate_email
+from flask import (Flask, current_app, flash, g, make_response, redirect,
+                   render_template, request, session, url_for)
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
+
+app.logger.setLevel(logging.DEBUG)
+
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+toolbar = DebugToolbarExtension(app)
+
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
+mail = Mail(app)
+
+
+# URLと実行する関数をマッピングする
+@app.route("/")
+def index() :
+	return "Hello, Flaskbook!"
+
+@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello, {name}!"
+
+# show_nameエンドポイントを作成する
+@app.route("/name/<name>")
+def show_name(name):
+	# 変数をテンプレートエンジンに渡す
+	return render_template("index.html", name=name)
+
+@app.route("/contact")
+def contact():
+    response = make_response(render_template("contact.html"))
+
+    response.set_cookie("flaskbook key", "flaskbook value")
+
+    session["username"] = "ichiro"
+
+    return response
+
+    return render_template("contact.html")
+
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        # form属性を使ってフォームの値を取得する
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+
+        is_valid = True
+
+        if not username:
+            flash("ユーザ名は必須です")
+            is_valid = False
+
+        if not email:
+            flash("メールアドレスは必須です")
+            is_valid = False
+
+        if not description:
+            flash("問い合わせ内容は必須です")
+            is_valid = False
+
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力してください")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+
+
+        # メールを送る(最後に実装)
+        send_email(
+            email,
+            "問い合わせありがとうございました。",
+            "contact_mail",
+            username=username,
+            description=description,
+        )
+
+        # contactエンドポイントへリダイレクトする
+        flash("問い合わせありがとうございました。")
+        return redirect(url_for("contact_complete"))
+
+    return render_template("contact_complete.html")
+
+
+with app.test_request_context():
+    print(url_for("index"))
+
+    print(url_for("hello-endpoint", name="world"))
+
+    print(url_for("show_name", name="ichiro"))
+
+
+# ここで呼び出すとエラーとなる
+# print(current_app)
+
+# アプリケーションコンテキストを取得してスタックヘpushする
+ctx = app.app_context()
+ctx.push()
+
+# current_appにアクセスが可能になる
+print(current_app.name)
+# >> apps.minimalapp.app
+
+# グローバルなテンポラリ領域に値を設定する
+g.connection = "connection"
+print(g.connection)
+# >> connection
+
+with app.test_request_context("/users?updated=true"):
+    # trueが出力される
+    print(request.args.get("updated"))
+
+
+def send_email(to, subject, template, **kwargs):
+	"""メールを送信する関数""" 
+	msg = Message( subject, recipients=[to])
+	msg.body = render_template(template + ".txt", **kwargs)
+	msg.html = render_template(template + ".html", **kwargs)
+	mail.send(msg)
